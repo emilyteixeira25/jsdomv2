b56a6006046e5903df3d7ce748481acbea9021a1
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2018-12-18 00:57:46 +0000

    used Array,isArray, Array.from, Array.from(titles).forEach

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ console.log(Array.isArray(titles));
 console.log(Array.isArray(Array.from(titles)));
 
 Array.from(titles).forEach(function(title){
-  console.log(title);
+    console.log(title);
 });
+
