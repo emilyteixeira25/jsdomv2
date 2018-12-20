d8ea974facf516e58630300add8d8d08b416ad00
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2018-12-20 23:32:09 +0000

    textContent

--- app.js
+++ app.js
@@ -6,14 +6,27 @@ list.addEventListener('click', function(e){
         const li = e.target.parentElement;
         list.removeChild(li);
     }
-    
 });
 
 //add book-list
 const addForm = document.forms['add-book'];
-
 addForm.addEventListener('submit',function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
-})
+
+//create elements
+const li = document.createElement('li');
+const bookName = document.createElement('span');
+const deleteBtn = document.createElement('span');
+
+//add content
+deleteBtn.textContent = 'delete';
+bookName.textContent = value;
+
+//append to document
+li.appendChild(bookName);
+li.appendChild(deleteBtn);
+li.appendChild(li);
+
+
+});
