c9bf9b35b2604a79e6e6bf1015bc67a3d9813372
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2018-12-21 00:13:18 +0000

    hideBox

--- app.js
+++ app.js
@@ -34,3 +34,12 @@ li.appendChild(li);
 
 
 });
+//hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change',function(e){
+    if(hideBox.checked){
+        list.style.display ="none";
+    }else{
+        list.style.display ="initial";
+    }
+})
